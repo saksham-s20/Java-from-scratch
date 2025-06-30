c993dca89611ce0ed38d4d8890db3a4846828b46
saksham-s20 <115614713+saksham-s20@users.noreply.github.com>
2025-06-30 23:26:03 +0530

    Add files via upload

---
+++ Practice1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Practice1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+        System.out.println("The number entered is: " + num);
+    }
+}
